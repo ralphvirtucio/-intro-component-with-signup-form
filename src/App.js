b6e3cc4c1bdc481e49ff52f4656d7ca19f654c36
ralphvirtucio <ralphvirtucio3@gmail.com>
2023-02-05 22:00:22 +0800

    Setup for frontend mentor challenge

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import './App.css';
+
 function App() {
   return (
     <div>
+      {/* Learn to code by watching others
+
+  See how experienced developers solve problems in real-time. Watching scripted tutorials is great, 
+  but understanding how developers think is invaluable. 
+
+  Try it free 7 days then $20/mo. thereafter
+
+  First Name 
+  Last Name 
+  Email Address
+  Password
+
+  Claim your free trial 
+
+  By clicking the button, you are agreeing to our Terms and Services  */}
       <h1>Hello World</h1>
     </div>
   );
