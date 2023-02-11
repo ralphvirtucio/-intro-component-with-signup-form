8217976adfed7a543906af6fc57f3c6c610e0b8c
ralphvirtucio <ralphvirtucio3@gmail.com>
2023-02-12 01:08:17 +0800

    WIP: Active input fields

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,34 @@
+import { useState } from 'react';
 import { Card } from '../UI/Card';
 import './Form.css';
 export const Form = () => {
+  const [firstName, setFirstName] = useState('');
+
+  const firstNameHandler = (e) => {
+    setFirstName(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    // let obj = {
+    //   firstName,
+    // };
+
+    // Validation of input fields
+  };
+
   return (
     <>
       <Card classname='form'>
-        <form>
+        <form onSubmit={submitHandler}>
           <div className='form-control'>
-            <input type='text' name='firstname' placeholder='First Name' />
+            <input
+              type='text'
+              name='firstname'
+              placeholder='First Name'
+              value={firstName}
+              onChange={firstNameHandler}
+            />
           </div>
           <button type='submit' className='btn--submit'>
             Claim your free trial
