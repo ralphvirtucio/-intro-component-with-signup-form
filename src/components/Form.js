237e9f9d7a1a6267c51e40f96a923296f6b1e0e1
ralphvirtucio <ralphvirtucio3@gmail.com>
2023-02-17 23:05:03 +0800

    Feat: Form - background-color, submit button and terms-agreements

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,16 @@
+import './Form.scss';
+
 export const Form = () => {
   return (
     <div>
-      <form>
+      <form className='form'>
         <div className='form-control'>
           <input type='text' name='firstname' placeholder='First Name' />
         </div>
         <button className='btn--submit'>Claim your free trial</button>
         <p className='terms-agreement'>
           By clicking the button, you are agreeing to our{' '}
-          <strong>Terms and Services</strong>
+          <strong> Terms and Services</strong>
         </p>
       </form>
     </div>
